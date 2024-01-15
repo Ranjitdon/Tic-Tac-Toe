6c228bdf440aafbfed52f1cce4366c6636ca907f
Ranjit Chaudhary <107805675+Ranjitdon@users.noreply.github.com>
2024-01-15 22:51:42 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset");
+let winner = document.querySelector(".winner");
+let message = document.querySelector("#msg");
+let newGame = document.querySelector("#newGame");
+
+const winPattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+turnO = true;
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("it was clicked");
+        if (turnO) {
+            box.innerText = "O";
+            box.style.color = "#48cae4";
+            turnO = false;
+        }
+        else {
+            box.innerText = "X";
+            box.style.color = "#ef233c";
+            turnO = true;
+        }
+        
+        box.disabled = true;
+        checkWinner();
+
+    });
+});
+
+let disableBtns = ()=>{
+    boxes.forEach((box)=>{
+        box.disabled = true;
+
+    });
+}
+
+let resetGame = ()=>{
+    enableBtns();
+    winner.classList.add("hide");
+    turnO = true;
+
+}
+let enableBtns = ()=>{
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+
+let showWinner = (pos)=>{
+    message.innerText = pos+" Won";
+    winner.classList.remove("hide");
+    disableBtns();
+
+}
+
+let checkWinner = () => {
+    for (let pattern of winPattern) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+        if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
+            if (pos1 === pos2 && pos1 === pos3) {
+                showWinner(pos1);
+
+            }
+        }
+    }
+}
+resetBtn.addEventListener("click",resetGame);
+newGame.addEventListener("click",resetGame);
